77a4981b63b7a3dc5804db71e82687b0a4d0ab45
AnabelenScript <ana0507belen@gmail.com>
2025-04-27 01:25:21 -0600

    Cambiando estilos del menu

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
-from scenes.Level1 import Level1
 from scenes.IntroSceneV1 import IntroScene
 
 if __name__ == "__main__":
-    game = Level1()
+    game = IntroScene()
     game.run()
